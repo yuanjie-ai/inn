610029f409b897feb52305f7660a805791f1a94a
yuanjie <313303303@qq.com>
2020-04-10 17:00:11 +0800

    [feature] add murmurhash

--- deepnn/utils/hashtrick.py
+++ deepnn/utils/hashtrick.py
@@ -15,14 +15,15 @@ from sklearn.utils.murmurhash import murmurhash3_32
 import java.nio.charset.StandardCharsets
 import com.google.common.hash.Hashing.murmur3_32
 
-  def hash(key: String = "key", value: String = "value", bins: Int = 10000): Int = {
+def hash(key: String = "key", value: String = "value", bins: Int = 10000): Int = {
     val hashValue: Int = murmur3_32.newHasher.putString(f"{key}:{value}", StandardCharsets.UTF_8).hash.asInt
     Math.abs(hashValue) % bins  
   }
 """
 
 
-def murmurhash(key="key", value="value", bins=10000):
+def murmurhash(key="key", value="value", bins=None):
     """key:value"""
-    hashValue = murmurhash3_32(f"{key}:{value}")
-    return abs(hashValue) % bins
+    _ = murmurhash3_32(f"{key}:{value}")
+    _ = abs(_)
+    return _ % bins if bins else _
